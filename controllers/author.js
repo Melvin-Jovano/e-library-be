445eabe06a3f5ac687f1f20387c20384c0518db9
mhduusop <mhduusop@gmail.com>
2023-02-02 22:55:36 +0700

    add delete and edit author

--- controllers/author.js
+++ controllers/author.js
@@ -38,3 +38,49 @@ export const createAuthor = async (req, res)=>{
         });
     }
 }
+
+export const updateAuthor = async (req,res)=>{
+    try {
+        const { authorId, authorName } = req.body
+        const author = await prisma.author.update({
+            where:{
+                id : authorId
+            },
+            data:{
+                name : authorName
+            },
+            select: {
+                id: true,
+                name: true,
+            }
+        })
+        return res.status(200).send({
+            message : "SUCCESS",
+            data: author
+        });
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send({
+            message : "An Error Has Occured",
+        });
+    }
+}
+
+export const deleteAuthor = async (req, res)=>{
+    try {
+        const { authorId } = req.body
+        await prisma.author.deleteMany({
+            where:{
+                id : authorId
+            }
+        })
+        return res.status(200).send({
+            message : "SUCCESS"
+        });
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send({
+            message : "An Error Has Occured",
+        });
+    }
+}
--- routes/author.js
+++ routes/author.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createAuthor, getAllAuthors, deleteAuthor } from "../controllers/author.js";
+import { createAuthor, getAllAuthors, updateAuthor, deleteAuthor } from "../controllers/author.js";
 
 const authorRoute = express.Router()
 
 authorRoute.get('/authors', getAllAuthors)
 authorRoute.post('/author', createAuthor)
+authorRoute.put('/author/:id', updateAuthor)
 authorRoute.delete('/author/:id', deleteAuthor)
 
 export default authorRoute;
